f1fed8f82f2aa0a3f992656082fbd6730c267056
sublee194 <sublee194@gmail.com>
2026-02-09 04:01:18 +0800

    Import loads of models

---
+++ Assets/CharacterAudition/TestCharacter.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class TestCharacter : MonoBehaviour
+{
+    private Animator _animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger("ChangeAnimation");
+        }
+    }
+
+
+}
